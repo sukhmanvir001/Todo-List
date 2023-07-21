7a904b2b80590455b6aeb9af0a8f22199b0dbed8
sukhmanvir001 <sukhmanvir.ca@gmail.com>
2023-07-21 01:11:36 -0400

    Create About.js

---
+++ src/MyComponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <div className="container d-flex flex-column">
+                <h3>About This Application</h3>
+                <p>
+The React Todo list application is a user-friendly task management tool that allows users to add, delete, and mark tasks as completed. Built with React and utilizing React Router for navigation, the application offers a simple and intuitive user interface. It ensures data persistence through local storage, enabling users to access their todos even after refreshing the page. The application's state is efficiently managed using React's useState hook, providing a seamless user experience. With the potential for future enhancements, this Todo list app proves to be a valuable tool for users seeking an organized approach to their daily tasks.</p>
+            </div>
+        </div>
+    )
+}
